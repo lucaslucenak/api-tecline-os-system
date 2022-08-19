f757accd687d43ac59957109158c426865bf6b97
Lucas Lucena <lucas.lucenak@gmail.com>
2022-08-19 12:37:33 -0300

    Add TechnicianService.java

---
+++ src/main/java/com/lucaslucena/APIosSystemTecLine/services/TechnicianService.java
@@ -0,0 +1,35 @@
+package com.lucaslucena.APIosSystemTecLine.services;
+
+import com.lucaslucena.APIosSystemTecLine.models.TechnicianModel;
+import com.lucaslucena.APIosSystemTecLine.repositories.TechnicianRepository;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TechnicianService {
+
+    final TechnicianRepository technicianRepository;
+
+    public TechnicianService(TechnicianRepository technicianRepository) {
+        this.technicianRepository = technicianRepository;
+    }
+
+    public TechnicianModel saveTechnician(TechnicianModel technician) {
+        return technicianRepository.save(technician);
+    }
+
+    public Optional<TechnicianModel> findTechnicianById(Long id) {
+        return technicianRepository.findById(id);
+    }
+
+    public List<TechnicianModel> findAllTechnicians() {
+        return technicianRepository.findAll();
+    }
+
+    public void deleteTechnicianById(Long id) {
+        technicianRepository.deleteById(id);
+    }
+}
