5d2a92abe8a7c6f38c48421e366d2503900513cc
Lucas Lucena <lucas.lucenak@gmail.com>
2022-08-19 17:29:33 -0300

    Add WebSecurityConfig

---
+++ src/main/java/com/lucaslucena/APIosSystemTecLine/config/security/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.lucaslucena.APIosSystemTecLine.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    final UserDetailsServiceImplementation userDetailsServiceImplementation;
+
+    public WebSecurityConfig(UserDetailsServiceImplementation userDetailsServiceImplementation) {
+        this.userDetailsServiceImplementation = userDetailsServiceImplementation;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors()
+                .and()
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
